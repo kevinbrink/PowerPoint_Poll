98d4853fdc930311ad8e135ff40f29a23565da3f
Mark Friedrich <frie0046@algonquinlive.com>
2014-09-16 09:29:15 -0400

    added code to add XML

--- CP3_plugin/Form1.cs
+++ CP3_plugin/Form1.cs
@@ -46,22 +46,50 @@ namespace CP3_plugin {
             // get data from dialog
             int slideIndex = ppSR.SlideIndex;
             string question = QuestionBox.Text;
-            bool tfCorrect;
+            string ans1, ans2, ans3, ans4, ans5;
+            string answerString = "";
+            string correctAnswer = "";
 
             if (format1.Checked){
                 // true or false                
                 if (TrueRadio.Checked){
-                    tfCorrect = true;
+                    correctAnswer = "true";
                 } else if (falseRadio.Checked) {
-                    tfCorrect = false;
+                    correctAnswer = "false";
                 }
                 else
                 {
                     // error must select correct answer first
+                    MessageBox.Show("There was no correct answer selected.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    return;
                 }
             }else if (format2.Checked){
                 // multiple choice
+                ans1 = textBox2.Text;
+                answerString = answerString + "<Answer1>" + ans1 + "</Answer1>";
+                if (checkBox1.Checked) correctAnswer = correctAnswer + "A,";
 
+                ans2 = textBox3.Text;
+                answerString = answerString + "<Answer2>" + ans2 + "</Answer2>";
+                if (checkBox2.Checked) correctAnswer = correctAnswer + "B,";
+
+                ans3 = textBox4.Text;
+                answerString = answerString + "<Answer3>" + ans3 + "</Answer3>";
+                if (checkBox3.Checked) correctAnswer = correctAnswer + "C,";
+
+                ans4 = textBox5.Text;
+                answerString = answerString + "<Answer4>" + ans4 + "</Answer4>";
+                if (checkBox4.Checked) correctAnswer = correctAnswer + "D,";
+
+                ans5 = textBox6.Text;
+                answerString = answerString + "<Answer5>" + ans5 + "</Answer5>";
+                if (checkBox5.Checked) correctAnswer = correctAnswer + "E";
+
+                if (!checkBox1.Checked || !checkBox2.Checked || !checkBox3.Checked || !checkBox4.Checked || !checkBox5.Checked)
+                {
+                    // error must select correct answer first
+                    MessageBox.Show("There was no correct answer selected.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
             }
 
             // create custom xml tag and insert
@@ -69,11 +97,14 @@ namespace CP3_plugin {
             //ppSR.CustomerData
 
             CustomXMLPart pollData = ppSR.CustomerData.Add();
-            pollData.LoadXML("<Customer><CustomerID>Customer #1</CustomerID></Customer>");
+            if (format1.Checked)
+            {
+                pollData.LoadXML("<Poll><PollSlide>" + slideIndex + "</PollSlide><PollType>True or False</PollType><PollQuestion>" + question + "</PollQuestion><PollCorrectAnswer>" + correctAnswer + "</PollCorrectAnswer></Poll>");
+            } else {
+                pollData.LoadXML("<Poll><PollSlide>" + slideIndex + "</PollSlide><PollType>Multiple choice</PollType><PollQuestion>" + question + "</PollQuestion><PollAnswers>" + answerString + "</PollAnswers><PollCorrectAnswer>" + correctAnswer + "</PollCorrectAnswer></Poll>");
+            }            
 
-            QuestionBox.Text = Convert.ToString(pollData.XML);
-
-            //Close();
+            Close();
         }
     }
 }
